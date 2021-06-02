93401188643a913eafda3e62b1dde9448996a275
David Toews <davejtoews@gmail.com>
2021-06-02 11:31:36 -0600

    ♻️ (model) uses link method rather than manually building link

--- src/FrontEndEditLink.php
+++ src/FrontEndEditLink.php
@@ -11,23 +11,23 @@ use SilverStripe\Dev\Debug;
 
 class FrontEndEditLink extends DataExtension
 {
-
-    public function EditLink(){
-
+    public function EditLink()
+    {
         if ($member = Security::getCurrentUser()) {
-            return DBField::create_field('HTMLFragment',"<a href='admin/pages/edit/show/".$this->owner->ID."/' style='
-            position: fixed;
-            z-index: 1000;
-            bottom: 0;
-            left: 0;
-            background: #f90;
-            padding: 0 3px;
-            font-size: 12px;
-            color: #fff;
-            ' target='_blank' rel='nofollow'>Edit</a>");
+            return DBField::create_field(
+                'HTMLFragment', "<a href='". $this->owner->CMSEditLink() . "/' style='
+                position: fixed;
+                z-index: 1000;
+                bottom: 0;
+                left: 0;
+                background: #f90;
+                padding: 0 3px;
+                font-size: 12px;
+                color: #fff;
+                ' target='_blank' rel='nofollow'>Edit</a>"
+            );
         }
 
         return NULL;
     }
-
 }
